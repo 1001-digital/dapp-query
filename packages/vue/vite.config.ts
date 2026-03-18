966efa10d1cf6a3839f871c10481bff54e10e080
jalil <jalil@1001.digital>
2026-03-18 12:31:55 +0100

    Fix build

--- packages/vue/vite.config.ts
+++ packages/vue/vite.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'index',
       formats: ['es'],
     },
     rollupOptions: {
